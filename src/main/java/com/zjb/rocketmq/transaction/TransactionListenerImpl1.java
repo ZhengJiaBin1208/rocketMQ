48866ace4cab399ef5f911720bd569c17a2753bf
ZhengJiaBin1208 <zhengjiabin@bestseller.com.cn>
2023-07-07 18:08:45 +0800

    1.rocketMQ分布式半事务生产者 2.分布式半事务监听

---
+++ src/main/java/com/zjb/rocketmq/transaction/TransactionListenerImpl1.java
@@ -0,0 +1,71 @@
+package com.zjb.rocketmq.transaction;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.producer.LocalTransactionState;
+import org.apache.rocketmq.client.producer.TransactionListener;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @ClassName TransactionListenerImpl
+ * @Description 事务监听
+ * @Author zhengjiabin
+ * @Date 2023/7/7 17:24
+ * @Version 1.0
+ **/
+@Slf4j
+public class TransactionListenerImpl1 implements TransactionListener {
+
+
+    /**
+     * When send transactional prepare(half) message succeed, this method will be invoked to execute local transaction.
+     * 当发送事务准备(半)消息成功时，将调用此方法执行本地事务。
+     *
+     * @param msg Half(prepare) message
+     * @param arg Custom business parameter
+     * @return Transaction state
+     */
+    @Override
+    public LocalTransactionState executeLocalTransaction(Message msg, Object arg) {
+        // 设置日期格式
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        // todo 执行本地事务 update A...
+        log.info("begin 本地事务, A服务器的TransactionId: {}",msg.getTransactionId());
+        log.info("update A ...(A账户减100块): {}",df.format(new Date()));
+        log.info("commit {}",df.format(new Date()));
+        // 以上代码都是李老师模拟--在数据库中对 A账户减100块。
+        // 如果上述代码发生异常或者执行不成功，认定失败了。
+        // return LocalTransactionState.ROLLBACK_MESSAGE;
+        // 本地事务执行成功返回
+        // return LocalTransactionState.COMMIT_MESSAGE;
+        // 这里是本地事务需要等待其他的(等待人脸识别的结果)
+        return LocalTransactionState.UNKNOW;
+    }
+
+    /**
+     * When no response to prepare(half) message. broker will send check message to check the transaction status, and this
+     * method will be invoked to get local transaction status.
+     * 当没有回应时，准备(半)消息。Broker将发送check消息来检查事务状态，并以此
+     * 方法将被调用以获取本地事务状态。
+     * 事务回查 默认是60s(不同的版本时间不同),一分钟检查一次
+     *
+     * @param msg Check message
+     * @return Transaction state
+     */
+    @Override
+    public LocalTransactionState checkLocalTransaction(MessageExt msg) {
+        // 设置日期格式
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        log.info("A服务器的本地事务回查, TransactionId: {}",msg.getTransactionId());
+        log.info("commit {}",df.format(new Date()));
+        // 这里检测到本地事务已经成功提交(检测 update A ...(A账户减100块) 这段SQL有没有执行成功)
+        // todo 情况3.1: 业务回查成功!
+        log.info("业务回查成功: 执行本地事务成功,确认消息");
+        return LocalTransactionState.COMMIT_MESSAGE;
+        // 这里是本地事务需要等待其他的(等待人脸识别的结果)
+        // return LocalTransactionState.UNKNOW;
+    }
+}
---
+++ src/main/java/com/zjb/rocketmq/transaction/TransactionProducer.java
@@ -0,0 +1,79 @@
+package com.zjb.rocketmq.transaction;
+
+import com.zjb.rocketmq.utils.ConfigUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.client.producer.SendStatus;
+import org.apache.rocketmq.client.producer.TransactionMQProducer;
+import org.apache.rocketmq.client.producer.TransactionSendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ClassName TransactionProducer
+ * @Description
+ * 分布式事务场景（用户A向用户B 转100块钱，用户A在A系统，用户B在B系统）
+ * 这里TransactionProducer  是A系统模拟用户A 在A系统进行金额扣减
+ * @Author zhengjiabin
+ * @Date 2023/7/7 17:43
+ * @Version 1.0
+ **/
+@Slf4j
+public class TransactionProducer {
+    public static void main(String[] args) throws MQClientException, InterruptedException {
+        // 创建事务监听器
+        TransactionListenerImpl transactionListener = new TransactionListenerImpl();
+
+        // 1.创建一个事务消息发送的生产者(TransactionProducer生产者分组: 针对就是分布式消息)
+        TransactionMQProducer producer = new TransactionMQProducer("TransactionProducer");
+        producer.setNamesrvAddr("127.0.0.1:9876");
+        // 创建线程池(JUC提供的)
+        ExecutorService executorService = new ThreadPoolExecutor(2, 5, 100, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(2000), new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r);
+                thread.setName("client-transaction-msg-check-thread");
+                return thread;
+            }
+        });
+        // 设置生产者回查线程池
+        producer.setExecutorService(executorService);
+        // 生产者设置监听器
+        producer.setTransactionListener(transactionListener);
+        // 启动消息生产者
+        producer.start();
+        // 1.发送事务消息(一定要用这个方法,sendMessageInTransaction)
+        try {
+            Message msg = new Message("TransactionTopic", null, ("用户A向用户B系统转100块").getBytes(RemotingHelper.DEFAULT_CHARSET));
+            SendResult sendResult = producer.sendMessageInTransaction(msg, null);
+            if (sendResult.getSendStatus() == SendStatus.SEND_OK){
+                // 设置日期格式
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                log.info("发送半事务消息成功: {}",df.format(new Date()));
+            }else {
+                log.info("发送半事务消息失败!!!");
+                return;
+            }
+        } catch (MQClientException | UnsupportedEncodingException e) {
+            log.info("发送半事务消息失败!!!");
+            e.printStackTrace();
+        }
+        // todo 启动事务补偿的消费监听
+        // 一些长时间等待的业务(比如输入密码,确认等操作):需要通过事务回查来处理
+        for (int i = 0; i < 1000; i++){
+            Thread.sleep(1000);
+        }
+        producer.shutdown();
+    }
+}
